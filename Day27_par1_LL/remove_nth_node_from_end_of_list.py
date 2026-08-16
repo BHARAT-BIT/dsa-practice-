d44de176add3a6e39f5306461a5023f84c55ab66
Bharat Saini <bharatsaini8476@gmail.com>
2026-08-16 22:33:29 +0530

    Some changes were made

--- Day27_par1_LL/remove_nth_node_from_end_of_list.py
+++ Day27_par1_LL/remove_nth_node_from_end_of_list.py
@@ -1,4 +1,11 @@
+class Node:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
 def remove_nth_from_end(head, n):
+
     dummy = Node(0)
     dummy.next = head
     fast = slow = dummy
