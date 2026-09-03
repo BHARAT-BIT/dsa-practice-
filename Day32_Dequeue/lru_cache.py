38be9837b256becea16a929341cf65c020166d9e
Bharat Saini <bharatsaini8476@gmail.com>
2026-09-03 21:44:34 +0530

    "LRU cache program modified"

--- Day32_Dequeue/lru_cache.py
+++ Day32_Dequeue/lru_cache.py
@@ -1,4 +1,5 @@
 class Node:
+
     def __init__(self, key, value):
         self.key = key
         self.value = value
@@ -7,50 +8,71 @@ class Node:
 
 
 class LRUCache:
-    def __init__(self, capacity):
-        self.capacity = capacity
-        self.cache = {}  # key -> Node
 
-        # dummy head and tail, permanently linked
+    def __init__(self, capacity):
+
+        self.capacity = capacity
+        self.cache = {}
+
         self.head = Node(0, 0)
         self.tail = Node(0, 0)
+
         self.head.next = self.tail
         self.tail.prev = self.head
 
-    def _remove(self, node):
-        # unlink node from its current position
-        prev_node = node.prev
-        next_node = node.next
-        prev_node.next = next_node
-        next_node.prev = prev_node
 
-    def _add_to_front(self, node):
-        # insert node right after head (most recently used spot)
-        node.prev = self.head
-        node.next = self.head.next
-        self.head.next.prev = node
-        self.head.next = node
+    def add(self, node):
+
+        node.prev = self.tail.prev
+        node.next = self.tail
+
+        self.tail.prev.next = node
+        self.tail.prev = node
+
+
+    def remove(self, node):
+
+        node.prev.next = node.next
+        node.next.prev = node.prev
+
 
     def get(self, key):
+
         if key not in self.cache:
             return -1
+
         node = self.cache[key]
-        self._remove(node)
-        self._add_to_front(node)
+
+        self.remove(node)
+        self.add(node)
+
         return node.value
 
-    def put(self, key, value):
-        if key in self.cache:
-            node = self.cache[key]
-            node.value = value
-            self._remove(node)
-            self._add_to_front(node)
-        else:
-            if len(self.cache) >= self.capacity:
-                lru_node = self.tail.prev
-                self._remove(lru_node)
-                del self.cache[lru_node.key]
 
-            new_node = Node(key, value)
-            self.cache[key] = new_node
-            self._add_to_front(new_node)
+    def put(self, key, value):
+
+        if key in self.cache:
+
+            node = self.cache[key]
+
+            self.remove(node)
+
+            node.value = value
+
+            self.add(node)
+
+        else:
+
+            node = Node(key, value)
+
+            self.cache[key] = node
+
+            self.add(node)
+
+            if len(self.cache) > self.capacity:
+
+                lru = self.head.next
+
+                self.remove(lru)
+
+                del self.cache[lru.key]
